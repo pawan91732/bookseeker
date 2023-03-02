bb2361293eccbeb36d64f516ea2fa7dbc23231b1
Pawan <76090869+pawan91732@users.noreply.github.com>
2023-03-02 09:18:49 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<img src="image/bookmark.png" class="logo" style="float: left;margin-right: 15px;"><a href="index.php" style="text-decoration: none;color: #3caea3;"><h1>BookSeeker.com</h1></a>
 		<h4>Sell | Buy | New | Old</h4>
 		<br><hr><br>
-		<h2>Copyright © 2019 | BookSeeker.com | All Rights Reserved | Design and Developed by-NARESH KUMAR</h2>
+		<h2>Copyright © 2019 | BookSeeker.com | All Rights Reserved | Design and Developed by- Pawan Gupta </h2>
 	</footer>
 	
 </body>
